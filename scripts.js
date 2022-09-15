8bf6d92621fc4e87bfe1dedd00d3c361e25ed7a9
Anton Langer <anton@Antons-MacBook-Air.local>
2022-09-15 12:29:20 -0600

    added section in dex.html, added styles to style.css, added new file form-styles.css, started building out scripts in script.js file

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// displays the popup of instructions
+function showPopup(){
+	document.querySelector(".popup").classList.toggle("active");
+}
+
+
+// when button is clicked to run report, this function fires
+// functional programming is used to prevent bugs
+function submitReport(){
+	let formData = getGoogleSheetData();
+}
+
+
+// grabs info from the form inputs and returns JSON
+function getFormData(){
+
+}
