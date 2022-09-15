657e48fe96791b903ea61add93d01c9d3dfa7f12
Anton Langer <anton@Antons-MacBook-Air.local>
2022-09-15 12:57:12 -0600

    started creating logic

--- scripts.js
+++ scripts.js
@@ -3,15 +3,49 @@ function showPopup(){
 	document.querySelector(".popup").classList.toggle("active");
 }
 
+// adds WYSIWYG editor
+window.addEventListener("load", ()=>{
+  ClassicEditor
+    .create( document.querySelector( '#other' ) )
+    .catch( error => {
+    console.error( error );
+  } );
+});
+
 
 // when button is clicked to run report, this function fires
 // functional programming is used to prevent bugs
 function submitReport(){
-	let formData = getGoogleSheetData();
+	const formData = getFormData();
+	displayReport(formData.googleSheet); 
 }
 
 
 // grabs info from the form inputs and returns JSON
 function getFormData(){
-
+	let formData = {
+		"googleSheet": document.querySelector("#google-sheet").value,
+		"sheetRow": document.querySelector("#google-sheet-num	").value,
+		"textHTML": document.querySelector(".ck.ck-content.ck-editor__editable.ck-rounded-corners.ck-editor__editable_inline").innerHTML
+	};
+	return formData;
 }
+
+
+// displays report after form data is processed
+function displayReport(formData){
+	document.querySelector("#main").classList.add("active");
+	if(!formData){
+		document.querySelector("first-page").classList.add("no-linkedin");
+		document.querySelector("second-page").classList.add("no-linkedin");
+	}
+}
+
+
+
+
+
+
+
+
+
